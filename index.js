20c36ee57488699037f4562e09fb7bc0cc57ce3a
Aslam Dhikrullahi <a.adepoju23@gmail.com>
2022-06-22 14:47:39 +0100

    first commit - calculator is operative

---
+++ index.js
@@ -0,0 +1,21 @@
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+let result;
+
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+alert(`${number1} ${operator} ${number2} = ${result}`);
